182e4755aeb767732381133bfe6e9dc98f7c26a4
khadinh <dalo2903@gmail.com>
2024-12-25 13:01:43 +0900

    new APIs for fetching graph

---
+++ src/pages/api/tag.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { spawnSync } from "child_process";
+
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const zk = spawnSync(
+    "zk",
+    [
+      "tag", "list",
+      "--format", "json",
+    ],
+    { maxBuffer: 1024 * 4096 }
+  )
+  var out = JSON.parse(zk.stdout.toString());
+  res.status(200).json(out);
+}
